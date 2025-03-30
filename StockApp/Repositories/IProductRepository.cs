3241871a6406416e5e43bb03db3882e2a9259276
Eduardo <eduardo.maticola@icloud.com>
2025-03-30 20:59:48 -0300

    feat: improvements in cshtml archives.

---
+++ StockApp/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using StockApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StockApp.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ StockApp/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockApp.Data;
+using StockApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using X.PagedList;
+using X.PagedList.Extensions;
+
+namespace StockApp.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DbContextStock _context;
+
+        public ProductRepository(DbContextStock context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductAsync(int pageNumber, int pageSize)
+        {
+            return await _context.Products.OrderBy(p => p.ProductId).ToPagedList(pageNumber, pageSize);
+        }
+    }
+}
